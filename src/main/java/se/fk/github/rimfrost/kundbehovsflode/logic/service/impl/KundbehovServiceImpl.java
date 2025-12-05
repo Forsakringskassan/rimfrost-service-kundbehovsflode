cfa491213ba4a03e3f89e70bbfc0d5114ef3b7a1
David Söderberg <david.soderberg@ductus.se>
2025-12-05 11:26:12 +0100

    fix: add missing ersattning to kundbehov and use correct variable for formanstyp

--- src/main/java/se/fk/github/rimfrost/kundbehovsflode/logic/service/impl/KundbehovServiceImpl.java
+++ src/main/java/se/fk/github/rimfrost/kundbehovsflode/logic/service/impl/KundbehovServiceImpl.java
@@ -1,6 +1,10 @@
 package se.fk.github.rimfrost.kundbehovsflode.logic.service.impl;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,16 +19,26 @@ import se.fk.github.rimfrost.kundbehovsflode.logic.dto.KundbehovCreateRequest;
 import se.fk.github.rimfrost.kundbehovsflode.logic.dto.KundbehovCreateResponse;
 import se.fk.github.rimfrost.kundbehovsflode.logic.dto.KundbehovGetRequest;
 import se.fk.github.rimfrost.kundbehovsflode.logic.dto.KundbehovGetResponse;
+import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ErsattningEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutableErsattningEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutableIndividEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutableKundbehovEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutableKundbehovsrollEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutablePeriodEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ImmutableProduceratResultatEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.IndividEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.KundbehovEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.KundbehovsrollEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.entity.PeriodEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.entity.ProduceratResultatEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.enums.AvsiktEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.BeloppstypEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.BerakningsgrundEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.BeslutsutfallEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.ErsattningsstatusEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.ErsattningstypEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.enums.KundbehovsstatusEntity;
+import se.fk.github.rimfrost.kundbehovsflode.logic.enums.PeriodiseringEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.enums.RollEntity;
 import se.fk.github.rimfrost.kundbehovsflode.logic.repository.KundbehovRepository;
 import se.fk.github.rimfrost.kundbehovsflode.logic.service.KundbehovService;
@@ -44,13 +58,20 @@ public class KundbehovServiceImpl implements KundbehovService
    @Override
    public KundbehovCreateResponse createKundbehov(KundbehovCreateRequest request)
    {
+      OffsetDateTime start = request.start();
+      OffsetDateTime slut = request.slut();
+      LocalDate startDate = start.toLocalDate();
+      LocalDate endDate = slut.toLocalDate();
+
+      long days = ChronoUnit.DAYS.between(start.toLocalDate(), slut.toLocalDate()) + 1;
+
       PeriodEntity periodEntity = ImmutablePeriodEntity.builder()
             .start(request.start())
             .slut(request.slut())
             .build();
 
       // TODO: Ta bort hårdkodning av namn
-      IndividEntity IndividEntity = ImmutableIndividEntity.builder()
+      IndividEntity individEntity = ImmutableIndividEntity.builder()
             .id(request.persnr())
             .fornamn("Lisa")
             .efternamn("Tass")
@@ -58,10 +79,60 @@ public class KundbehovServiceImpl implements KundbehovService
 
       KundbehovsrollEntity kundbehovsrollEntity = ImmutableKundbehovsrollEntity.builder()
             .id(UUID.randomUUID())
-            .individ(IndividEntity)
+            .individ(individEntity)
             .roll(RollEntity.AGARE)
             .yrkande(true)
             .build();
+
+      List<ErsattningEntity> ersattningar = new ArrayList<>();
+      for (int i = 0; i < days; i++) {
+            LocalDate currentDate = startDate.plusDays(i);
+
+            OffsetDateTime franOchMed;
+            OffsetDateTime tillOchMed;
+
+            if (i == 0) {
+                  // First day
+                  franOchMed = start;
+
+                  if (startDate.equals(endDate)) {
+                        tillOchMed = slut;
+                  } else {
+                        tillOchMed = currentDate.atTime(LocalTime.MAX).atOffset(start.getOffset());
+                  }
+            } else if (currentDate.equals(endDate)) {
+                  // Last day
+                  franOchMed = currentDate.atStartOfDay().atOffset(slut.getOffset());
+                  tillOchMed = slut;
+            } else {
+                  // Middle day
+                  franOchMed = currentDate.atStartOfDay().atOffset(start.getOffset());
+                  tillOchMed = currentDate.atTime(LocalTime.MAX).atOffset(start.getOffset());
+            }
+
+            ProduceratResultatEntity pre = ImmutableProduceratResultatEntity.builder()
+                  .id(UUID.randomUUID())
+                  .version("1.0")
+                  .franOchMed(franOchMed)
+                  .tillOchMed(tillOchMed)
+                  .status(ErsattningsstatusEntity.PLANERAT)
+                  .build();
+
+            ErsattningEntity ersattningEntity = ImmutableErsattningEntity.builder()
+                  .id(UUID.randomUUID())
+                  .belopp(40000)
+                  .berakningsgrund(BerakningsgrundEntity.LON)
+                  .beloppstyp(BeloppstypEntity.INKOMSTBASERAD)
+                  .ersattningstyp(ErsattningstypEntity.HUNDBIDRAG)
+                  .periodisering(PeriodiseringEntity.DAG)
+                  .omfattning(100)
+                  .beslutsutfall(BeslutsutfallEntity.FU)
+                  .avslagsanledning("")
+                  .produceratResultat(List.of(pre))
+                  .build();
+
+            ersattningar.add(ersattningEntity);
+      }
 
       KundbehovEntity kundbehovEntity = ImmutableKundbehovEntity.builder()
             .id(UUID.randomUUID())
@@ -73,6 +144,7 @@ public class KundbehovServiceImpl implements KundbehovService
             .avsikt(AvsiktEntity.NY)
             .andringsorsak("")
             .kundbehovsroll(List.of(kundbehovsrollEntity))
+            .ersattning(ersattningar)
             .build();
 
       kundbehovRepository.save(kundbehovEntity);
